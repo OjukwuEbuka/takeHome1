0f9d07dbb0fa1749929d9d49c876b939942ccc7a
OjukwuEbuka <ebukaojukwu23@gmail.com>
2021-03-20 09:47:54 +0100

    Completed Timer feature

--- src/pages/Timer/index.js
+++ src/pages/Timer/index.js
@@ -1,6 +1,6 @@
 // Globals
 import "./styles.scss";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 // Components
 import { Button } from "components/Button";
@@ -19,8 +19,48 @@ function Expired() {
 function Timer() {
   // Hooks - state
   const [counter, setCounter] = useState(0);
+  const [displayTime, setDisplayTime] = useState('0:00');
+  const [started, setStarted] = useState(false);
 
   // TODO: implement counter...
+
+  // set timer interval
+  let countTimer;
+
+  //handle reset button click
+  const handleReset = () => {
+    setCounter(59);
+    setDisplayTime('1:00');
+    setStarted(false);
+    clearInterval(countTimer);
+  }
+
+  // handle start button click
+  const handleStart = () => {
+    setStarted(true);
+  }
+
+  useEffect(()=>{
+    if(counter > 0 && started){
+      let showTime = counter;
+      if(counter < 10){
+        showTime = `0${counter}`;
+      }
+      setDisplayTime(`0:${showTime}`);
+      countTimer = setInterval(()=>{
+        setCounter(counter => counter - 1);
+        console.log(counter);
+      }, 1000);
+    } 
+
+    if(counter === 0){
+      setDisplayTime(`0:00`);
+    }
+
+    return () => clearInterval(countTimer);
+    
+  }, [counter, started])
+  
 
   // Render
   return (
@@ -28,12 +68,12 @@ function Timer() {
       <h1>Timer</h1>
 
       <div className="aura-page-content">
-        <div className="aura-timer-clock">0:00</div>
+        <div className="aura-timer-clock">{displayTime}</div>
         {counter <= 0 ? <Expired /> : null}
 
         <div className="aura-timer-buttons">
-          <Button>Start</Button>
-          <Button>Reset</Button>
+          <Button onClick={handleStart}>Start</Button>
+          <Button onClick={handleReset}>Reset</Button>
         </div>
       </div>
     </div>
